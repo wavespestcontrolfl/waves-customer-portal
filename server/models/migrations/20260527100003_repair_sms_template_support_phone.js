3b245f8f54b7a977d0f01c0868391a05d0d610ff
wavespestcontrolfl <contact@wavespestcontrol.com>
2026-05-26 07:39:22 -0400

    Fix SMS template support phone

---
+++ server/models/migrations/20260527100003_repair_sms_template_support_phone.js
@@ -0,0 +1,63 @@
+const SUPPORT_PHONE = '(941) 297-5749';
+const LOCATION_PHONE = '(941) 318-7612';
+
+const PHONE_TEMPLATE_KEYS = [
+  'billing_reminder',
+  'autopay_charge_failed',
+  'autopay_retry_failed',
+  'autopay_retry_final_failed',
+  'referral_invite',
+  'auto_renewal_30_60_day_notice',
+  'seasonal_alert',
+  'estimate_auto_renewed',
+];
+
+exports.up = async function up(knex) {
+  if (!(await knex.schema.hasTable('sms_templates'))) return;
+
+  const cols = await knex('sms_templates').columnInfo();
+  const now = new Date();
+
+  for (const templateKey of PHONE_TEMPLATE_KEYS) {
+    const template = await knex('sms_templates')
+      .where({ template_key: templateKey })
+      .first();
+
+    if (!template?.body || !template.body.includes(LOCATION_PHONE)) continue;
+
+    const update = {
+      body: template.body.split(LOCATION_PHONE).join(SUPPORT_PHONE),
+    };
+    if (cols.updated_at) update.updated_at = now;
+
+    await knex('sms_templates')
+      .where({ template_key: templateKey })
+      .update(update);
+  }
+
+  const estimateExtended = await knex('sms_templates')
+    .where({ template_key: 'estimate_extended' })
+    .first();
+
+  if (estimateExtended) {
+    const variables = Array.isArray(estimateExtended.variables)
+      ? estimateExtended.variables
+      : JSON.parse(estimateExtended.variables || '[]');
+    const cleanedVariables = variables.filter((variable) => variable !== 'days_added');
+
+    if (cleanedVariables.length !== variables.length) {
+      const update = {
+        variables: JSON.stringify(cleanedVariables),
+      };
+      if (cols.updated_at) update.updated_at = now;
+
+      await knex('sms_templates')
+        .where({ template_key: 'estimate_extended' })
+        .update(update);
+    }
+  }
+};
+
+exports.down = async function down() {
+  // Copy cleanup only. Do not reintroduce the old customer-support phone.
+};
