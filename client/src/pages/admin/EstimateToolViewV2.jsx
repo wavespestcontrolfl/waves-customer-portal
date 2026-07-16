474c3bdf0bd21a288cbdf8a8738433fbf3ff2245
wavespestcontrolfl <contact@wavespestcontrol.com>
2026-07-16 08:21:04 -0400

    Fix pricing PR lint gate

--- client/src/pages/admin/EstimateToolViewV2.jsx
+++ client/src/pages/admin/EstimateToolViewV2.jsx
@@ -1954,7 +1954,6 @@ export default function EstimateToolViewV2({
       setTimeout(() => clearInterval(interval), 5000);
     };
     document.head.appendChild(script);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function initAutocomplete() {
@@ -2555,7 +2554,6 @@ export default function EstimateToolViewV2({
     return () => {
       cancelled = true;
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [editEstimateId]);
 
   function exitEditMode() {
@@ -2699,7 +2697,6 @@ export default function EstimateToolViewV2({
     } catch {
       /* ignore */
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Prefill bait-station footprint from house sqft, but keep BoraCare and
@@ -2749,13 +2746,11 @@ export default function EstimateToolViewV2({
     } catch {
       /* ignore */
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     const t = setTimeout(() => searchCustomers(customerSearch), 300);
     return () => clearTimeout(t);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customerSearch]);
 
   const [enrichedProfile, setEnrichedProfile] = useState(null);
@@ -2798,7 +2793,6 @@ export default function EstimateToolViewV2({
     } catch {
       setVerifySaveState("error");
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [form.address, form.homeSqFt, form.lotSqFt, form.stories, form._storiesEdited, enrichedProfile]);
 
   const resolveFleaExteriorDefault = useCallback((currentForm = form) => {
